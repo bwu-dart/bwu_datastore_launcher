fa680c71f343ddc3884e470cd995e8fd0c694203
zoechi <gzoechi@gmail.com>
2015-06-07 17:57:27 +0200

    - attempt to install gcd and gcloud on Travis

--- tool/grind.dart
+++ tool/grind.dart
@@ -1,3 +1,10 @@
 library bwu_datastore_launcher.tool.grind;
 
-export 'package:bwu_utils_dev/grinder/default_tasks.dart';
+export 'package:bwu_utils_dev/grinder/default_tasks.dart' hide main, testWeb;
+import 'package:bwu_utils_dev/grinder/default_tasks.dart'
+    show grind, testTask, testTaskImpl;
+
+main(List<String> args) {
+  testTask = ([_]) => testTaskImpl(['vm']);
+  grind(args);
+}
